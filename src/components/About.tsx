fac11525e740424e180959799c3a499ea9ecd54e
Raziq Ramli <42981908+raziqraif@users.noreply.github.com>
2022-08-26 22:09:27 +0800

    Update about

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -94,7 +94,7 @@ export class About extends React.Component {
           </div>
           <div className={"d-flex d-flex-row"}>
             <div style={textStyle}>
-              I build scientific software and compilers. Previously, I developed research tools for studying global sustainability issues. Currently, I am at Purdue RSSys Lab, designing a pointer analysis framework for modular systems.
+              I build scientific software and compilers. Previously, I developed analysis tools that facilitate research on global sustainability issues. Right now, I am at Purdue RSSys Lab, designing a pointer analysis framework for modular systems.
               {/* I will also be joining the Compiler Engineering team at Qualcomm. */}
             </div>
           </div>
