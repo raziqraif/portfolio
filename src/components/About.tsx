a9bdfe8ab5e0ebc8c4fec2b01524485b3760a7aa
Raziq Ramli <42981908+raziqraif@users.noreply.github.com>
2022-09-02 01:05:51 -0700

    Update about

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -94,8 +94,7 @@ export class About extends React.Component {
           </div>
           <div className={"d-flex d-flex-row"}>
             <div style={textStyle}>
-              I am an incoming Compiler Engineer at Qualcomm. I also have affiliation with Purdue RSSys Lab, where I am currently developing a framework for improving pointer analysis performance. Before my current role, I have primarily designed scientific software for sustainability research groups throughout my undergraduate at Purdue University. 
-              {/* I will also be joining the Compiler Engineering team at Qualcomm. */}
+              I am an incoming Compiler Engineer at Qualcomm. I am also involved with Purdue RSSys Lab, developing a technique for improving pointer analysis performance. Before my current role, I was an undergraduate at Purdue University, where I primarily designed scientific software for sustainability research groups.
             </div>
           </div>
           <div
