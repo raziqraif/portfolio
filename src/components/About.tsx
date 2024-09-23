1d07e35e1db59933547c5eac3d77210d9456525e
Raziq Ramli <42981908+raziqraif@users.noreply.github.com>
2024-09-23 01:16:59 -0700

    Update about

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -94,7 +94,10 @@ export class About extends React.Component {
           </div>
           <div className={"d-flex d-flex-row"}>
             <div style={textStyle}>
-              I am a graphics compiler engineer at Qualcomm working on OpenCL code generation and optimization. Generally, I am interested in using mathematical theories to make correct, performant, and secure systems. I am also interested in how computing can help us answer questions regarding the human brain, in particular, regarding cognition. Previously, I was an undergraduate at Purdue University, focusing on algorithmic foundations and systems programming.
+              I am a graphics compiler engineer at Qualcomm working on OpenCL code generation and optimization.
+               Generally, I am interested in using mathematical theories to make correct, performant, and secure systems.
+                I am also interested in learning about the human brain, in particular, cognition, through the lens of computing.
+                Previously, I was an undergraduate at Purdue University, focusing on algorithmic foundations and systems programming.
             </div>
           </div>
           <div
